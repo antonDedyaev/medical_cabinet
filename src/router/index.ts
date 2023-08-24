68e7174b7485c6d5bbebb2080b4aa14a73fa74d2
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-08-24 12:37:51 +0300

    added url to router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,7 +23,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory("/medical_cabinet/"),
   routes,
 });
 
